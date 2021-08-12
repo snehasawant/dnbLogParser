74b0978ad67696f88b684a237f4aaea65446e14c
snehasawant <sawantsneha147@gmail.com>
2021-08-12 14:11:42 +0530

    Update JSONLogParserTest.java

--- src/test/java/com/dnb/Parser/impl/JSONLogParserTest.java
+++ src/test/java/com/dnb/Parser/impl/JSONLogParserTest.java
@@ -1,4 +1,15 @@
 package com.dnb.Parser.impl;
 
-public class JSONLogParserTest {
+import com.dnb.Parser.TestSuite;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JSONLogParserTest extends TestSuite {
+
+    @Test
+    public void testparseInput()
+    {
+        JSONLogParser jsonLogParser = new JSONLogParser();
+       // Assertions.assertTrue(jsonLogParser.parseInput(spark,"D:\\projectArea\\test", "D:\\projectArea\\activity.xsd", "D:\\projectArea\\activitycode.csv").size()>0);
+    }
 }
