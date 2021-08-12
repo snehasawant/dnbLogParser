926d8d0dafa775f8ee1eceaaae53b9fd69e5432a
snehasawant <sawantsneha147@gmail.com>
2021-08-12 14:12:06 +0530

    Update XMLLogParserTest.java

--- src/test/java/com/dnb/Parser/impl/XMLLogParserTest.java
+++ src/test/java/com/dnb/Parser/impl/XMLLogParserTest.java
@@ -1,4 +1,14 @@
 package com.dnb.Parser.impl;
 
-public class XMLLogParserTest {
+import com.dnb.Parser.TestSuite;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class XMLLogParserTest extends TestSuite {
+    @Test
+    public void testparseInput()
+    {
+        XMLLogParser xmlLogParser = new XMLLogParser();
+       // Assertions.assertTrue(xmlLogParser.parseInput(spark,"D:\\projectArea\\test", "D:\\projectArea\\activity.xsd", "D:\\projectArea\\activitycode.csv").size()>0);
+    }
 }
